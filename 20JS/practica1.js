27708576e01071ed94aa92aaf725edc9232bdfd2
rechy <rechy.mr@gmail.com>
2022-06-29 20:17:56 -0500

    update290622

---
+++ 20JS/practica1.js
@@ -0,0 +1,47 @@
+
+let lluvia = true
+if(lluvia=true){
+    console.log("Me quedo a ver una peli")}
+    else{
+        console.log("saco a pasear a mi perro")
+    }
+
+
+
+
+
+
+
+    
+
+let edad=0
+let sem =0
+if(edad<60){
+    if(sem=750){
+        console.log("se puede pensionar")
+    }
+}
+
+
+
+var opcion=0
+console.log("menu principal")
+console.log("opcion1:retiro de efectivo")
+console.log("opcion2:consulta de movimientos")
+console.log("opcion3:impresion de saldos")
+console.log("opcion4:Salir")
+switch(opcion){
+    case "1":
+        console.log("retiro de efectivo")
+        break;
+    case"2":
+        console.log("consulta de movimientos")
+        break;
+    case"3":
+        console.log("impresion de saldos")
+        break;
+    case"4":
+        console.log("Salir")
+    default:
+        console.log("Cancelar")
+}
