777fb98f7c9b265fb6550cd59c14bebbbd03e92e
rechy <rechy.mr@gmail.com>
2022-07-06 17:47:44 -0500

    update0607222

--- js/ejercicioCiclos.js
+++ js/ejercicioCiclos.js
@@ -72,3 +72,84 @@ for( let i=r.length-1; i>0; i--){
 }
 console.log(rReverse)
 */
+
+
+/*Ejercicio 1
+Escribir un programa de arroje la suma de los múltiplos de 3, 5 o 7 que hay entre 1 y 100
+Resolver con For, While y do While*/
+
+
+for(i=1; (i>1 && i<100); i++){
+    
+}
+
+
+
+
+
+/*Ejercicio 2
+Pedir el usuario 1 número entre 1 y 100
+sumar todos los números entre el 1 y el numero ingresado
+Mandar un alert con el total
+Resolver con For, While y do While
+Por ejemplo: 5
+->  15*/
+
+/*
+let num= parseInt(prompt("Escribe un número entre el 1 y 100"))
+let sum = 1
+let i = 2
+*/
+
+/*Utilizando  DO WHILE
+if(num>100){
+    window.alert("Número invalido")
+}
+else if(num<=100){
+do{
+    sum = sum + i; 
+    i++;
+}
+while(i <= num)
+window.alert(sum)
+}
+//termina ejercicio 
+*/
+/*
+//UTILIZANDO FOR
+if(num>100){
+    window.alert("Número invalido")
+}
+else if(num<=100{
+for (i=2; i<=num; i++){
+   sum = sum + i;
+}
+window.alert(sum)
+}
+//Termina FOR
+*/
+/*
+//WHILE
+if(num>100){
+    window.alert("Número invalido")
+}
+else if(num<=100){
+while (i <= num){  
+    sum = sum + i; 
+    i++;
+}
+window.alert(sum)
+}
+//termina WHILE 
+*/
+ 
+
+/*Ejercicio 3
+pedir una oracion al usuario
+Contar las letras "a", "e" y espacios,
+Resolver con For, While y do While
+Por ejemplo 'hola kodErs'
+ ->  A -> 1
+ ->  E -> 1
+ ->  Espacios -> 1 */
+
